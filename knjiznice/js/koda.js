716e1a20f8fe222ab81be08c794e126d44696ec2
Primoz Hrovat <primoz.hrovat.96@gmail.com>
2016-06-04 01:08:14 +0200

    Implementirane resitve

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -5,6 +5,8 @@ var queryUrl = baseUrl + '/query';
 var username = "ois.seminar";
 var password = "ois4fri";
 
+var baseUrlWiki = "https://sl.wikipedia.org/w/api.php"
+
 
 /**
  * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
@@ -31,44 +33,34 @@ function getSessionId() {
  * @return ehrId generiranega pacienta
  */
 function generirajPaciente(callback) {
-	var napaka;
-	var ehrid;
-	var ime, priimek, datum_rojstva;
-	for(var i = 1; i<4; i++){
-		switch(i){
-			
-			//bolnik, povisan krvni tlak
-			case 1:
-				ime = "Janez";
-				priimek = "Bolan";
-				spol = "Moški";
-				datum_rojstva = "1954-02-08T06:20";
-				var meritev1;
-				ehrid = novEHR(ime, priimek, spol, datum_rojstva);
-				break;			
-			//sportnica
-			case 2:
-				ime = "Marija";
-				priimek = "Kovač"
-				spol = "Ženski";
-				datum_rojstva = "1981-05-15T02:15";
-				ehrid = novEHR(ime, priimek, spol, datum_rojstva);
-				break;
-			//mladenic
-			case 3:
-				ime = "Matic";
-				priimek = "Oblak";
-				spol = "Moški";
-				datum_rojstva = "1998-01-01T14:37";
-				ehrid = novEHR(ime, priimek, spol, datum_rojstva);
-				break;
-		}
-
-		generirajPodatke(i, ehrid, function(stanje){
-					if(stanje)
-						napaka = false;
+	var napaka;		
+		novEHR("Janez", "Bolan", "Moški", "1954-02-08T06:20", function(ehr){
+			generirajPodatke(1, ehr, function(stanje){
+				console.log(ehr);
+				if(stanje)
+					napaka = false;
+				else
+					napaka = true;
+			});
 		});
-	}
+		novEHR("Marija", "Kovač", "Ženski", "1981-05-15T02:15", function(ehr){
+			generirajPodatke(2, ehr, function(stanje){
+				console.log(ehr);
+				if(stanje)
+					napaka = false;
+				else
+					napaka = true;
+			});
+		});
+		novEHR("Matic", "Oblak", "Moški", "1998-01-01T14:37", function(ehr){
+			generirajPodatke(3, ehr, function(stanje){
+				console.log(ehr);
+				if(stanje)
+					napaka = false;
+				else
+					napaka = true;
+			});
+		});
 	callback(napaka);
   // TODO: Potrebno implementirati
 }
@@ -79,6 +71,8 @@ function generirajPodatke(stPacienta, ehrid, callback){
 	var diastolicni_spodnja;
 	var diastolicni_zgornja;
 	var polozaj;
+	var datum = new Date();
+
 	switch(stPacienta){
 		case 1:
 			sistolicni_spodnja = 139;
@@ -100,19 +94,21 @@ function generirajPodatke(stPacienta, ehrid, callback){
 			break;
 	}
 	var sis, dia;
+	console.log(sistolicni_zgornja + " " + stPacienta);
 	for(var i = 0; i<15; i++){
 		sis = Math.floor(Math.random()* (sistolicni_zgornja - sistolicni_spodnja) + sistolicni_spodnja + 1);
 		dia = Math.floor(Math.random()* (diastolicni_zgornja - diastolicni_spodnja) + diastolicni_spodnja + 1);
 		polozaj = "*Sitting(en)";
-		if(ehrid== null){ ehrid=$("#ehrid_izpis").val(); pacienti[stPacienta-1].ehrid = ehrid; }
-		vnosPodatkov(ehrid, sis, dia, polozaj, null, function(status){
+		var datumUra = datum.getFullYear() + "-" + datum.getMonth() + "-" + (datum.getDate() + i) + "T"
+						+ datum.getHours() + ":" + datum.getMinutes();
+		vnosPodatkov(ehrid, sis, dia, polozaj, datumUra, function(status){
 			if(status) callback(true);
 			else callback(false);
 		});
 	}
 }
 
-function novEHR(ime, priimek, spol, datum_rojstva){
+function novEHR(ime, priimek, spol, datum_rojstva, callback){
 	var sessionID = getSessionId();
 	var sporocilo = "";
 	var ehrId;
@@ -143,7 +139,12 @@ function novEHR(ime, priimek, spol, datum_rojstva){
                           "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
                           	Uspešno kreiran EHR za " + ime + " " + priimek + ":\n" + ehrId + ".</div>");
 		                    $("#ehrid_izpis").val(ehrId);
-		                    
+		                   	switch(ime){
+		                   		case "Janez": pacienti.JanezBolan.ehrid = ehrId; break;
+		                   		case "Marija": pacienti.MarijaKovac.ehrid = ehrId; break;
+		                   		case "Matic": pacienti.MaticOblak.ehrid = ehrId; break;
+		                   } 
+		                   	callback(ehrId);
 		                }
 		            },
 		            error: function(err) {
@@ -151,17 +152,16 @@ function novEHR(ime, priimek, spol, datum_rojstva){
                           "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
                           	Napaka " +
                     		JSON.parse(err.responseText).userMessage + "!</div>");
+		            	callback(false);
 		            }
 		        });
 		    }
 		});
-
-		return ehrId;
 }
 
 function vnosPodatkov(ehrid, sistolicni, diastolicni, polozaj, datumUra, callback){
 	var sessionId = getSessionId();
-	if(!datumUra){
+	if(datumUra == null){
 		var datum = new Date();
 		var datumUra = datum.getFullYear() + "-" + datum.getMonth() + "-" + datum.getDate() + "T"
 						+ datum.getHours() + ":" + datum.getMinutes();
@@ -187,8 +187,7 @@ function vnosPodatkov(ehrid, sistolicni, diastolicni, polozaj, datumUra, callbac
 		templateId: "Vital Signs",
 		format: "FLAT",
 	};
-	console.log($.param(parametri));
-
+	
 	$.ajax({
 		url: baseUrl + "/composition?" + $.param(parametri),
 		type: "POST",
@@ -197,8 +196,7 @@ function vnosPodatkov(ehrid, sistolicni, diastolicni, polozaj, datumUra, callbac
 		success: function(rezultat){
 			$("#sporocilo_vnos").html("<div class='alert alert-success fade in'>" +
                           "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
-                          	Uspešno vneseno</div>");
-			callback(true);
+                          	Uspešno vneseno" + "</div>");
 		},
 		error: function(napaka){
 			$("#sporocilo_vnos").html("<div class='alert alert-danger fade in'>" +
@@ -219,7 +217,7 @@ function izpisPodatkov(ehrid, callback){
 		headers: {"Ehr-Session": sessionid},
 		success: function (podatki){
 			var oseba = podatki.party;
-			$("#accordion").html("<h5>Podatki za " + oseba.firstNames + " " + oseba.lastNames + "</h5>");
+			$("#accordion").html("<h5>Podatki za " + oseba.firstNames + " " + oseba.lastNames + "</br>" + ehrid + "</h5>");
 			$.ajax({
 				url: baseUrl + "/view/" + ehrid + "/blood_pressure",
 				type: "GET",
@@ -227,24 +225,26 @@ function izpisPodatkov(ehrid, callback){
 				success: function(rezultat){
 					if(rezultat.length>0){
 						for(var i in rezultat){
-							console.log(rezultat[i]);
+							sis = rezultat[i].systolic;
+							dia = rezultat[i].diastolic;
+							dt = rezultat[i].time;
+							datum = new Date(dt);
 							izpis = '<div class="panel panel-default">\
             					<div class="panel-heading">\
               					<h4 class="panel-title">\
                					<a data-toggle="collapse" data-parent="#accordion" href="#collapse' + (parseInt(i)+1) +'">\
               					Meritev' + (parseInt(i)+ 1) + '</a></h4>\
             					</div><div id="collapse' + (parseInt(i)+1) + '" class="panel-collapse collapse">\
-              					<div class="panel-body"><ul><li>Sistolični: ' + rezultat[i].systolic + 
-              					'</li><li>Diastolični: ' + rezultat[i].diastolic + '</li></ul></div></div></div>';
+              					<div class="panel-body"><ul><li>Sistolični: ' + sis + 
+              					'</li><li>Diastolični: ' + dia + '</li><li>Datum: ' + datum +'</li></ul></div></div></div>';
               				$("#accordion").append(izpis);
+              				napolniTabelo(parseInt(i), sis, dia, dt);
           				}
-
-
-						
-						
+          				preveriTlak();
+          				drawCrosshairs();					
 					}
 					else
-						$("#accordion").html("<div class='alert alert-danger fade in'>" +
+						$("#accordion").append("<div class='alert alert-danger fade in'>" +
                           "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
                           	Ni zapisov </div>");
 				},
@@ -259,43 +259,117 @@ function izpisPodatkov(ehrid, callback){
 	callback(true);
 }
 
+function napolniTabelo(i, sis, dia, dt){
+	var dat = new Date(dt.split("T")[0]);
 
-var pacienti = [
-	{
-		ime: "Janez Bolan",
-		ehrid: "8b2514c4-2d2e-45b1-9c0e-dba790c30bfc"
-	},
-	{
-		ime: "Marija Kovač",
-		ehrid: "ab825179-8a1f-47d0-9062-2a782c263ba7"
-	},
-	{
-		ime: "Matic Oblak",
-		ehrid: "cae5c359-5ed3-4b18-a75a-edb5a476aa19"
+	tabela[i] = [dat, sis, dia];
+	console.log(tabela[i]);
+}
+
+function preveriTlak(){
+	var sis = 0;
+	var dia = 0;
+	var st = 0;
+	for(var i in tabela){
+		sis += tabela[i][1];
+		dia += tabela[i][2];
+		st++;
 	}
-];
+	sis/=st;
+	dia/=st;
+	if(sis > 140 || dia > 90){
+		podatkiWiki();
+		$("#accordion").append("<div class='alert alert-danger fade in'>" +
+                          "<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>\
+                          	Povišan krvni tlak! Priporočamo obisk zdravnika." + "</div>");
+	}
+	else
+		$("#info_box").empty();
+}
+
+
+function drawCrosshairs() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'X');
+      data.addColumn('number', 'Sistolični');
+      data.addColumn('number', 'Diastolični');
+
+      data.addRows(tabela);
+      var options = {
+        hAxis: {
+          title: 'Datum'
+        },
+        vAxis: {
+          title: 'mm[Hg]'
+        },
+        colors: ['#a52714', '#097138'],
+        crosshair: {
+          color: '#000',
+          trigger: 'selection'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('graf'));
+
+      chart.draw(data, options);
+      chart.setSelection([{row: 38, column: 1}]);
+
+}
+
+
+function podatkiWiki(){
+
+	$.getJSON(baseUrlWiki+"?action=query&format=json&callback=?", 
+		{titles:"Visok krvni tlak", prop: "extracts", exintro: "", explaintext: ""},
+		function(data) {
+			var stran = data.query.pages;
+			var extract = stran[245675].extract;
+			extract = extract.replace("}", "");
+			
+			$("#info_box").html('<blockquote><p class="text-justify small">'+extract+'</p>\
+				<footer>Wikipedia</footer></blockquote>');
+		});
+}
+
+
+var tabela = [
+		[0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0],
+		[0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0],
+		];
+
+
+
+var pacienti = {
+	
+	"JanezBolan" : {
+		ehrid: "02b2181e-2a22-4862-95c0-935eb113bbc9"
+	},
+	"MarijaKovac" : {
+		ehrid: "cffa8328-b11e-493b-81e9-458d617fceb8"
+	},
+	"MaticOblak" : {
+		ehrid: "515227e2-132b-4f04-9e8e-ceedb097e69a"
+	}
+};
+
+
+
 
 $(function(){
 
-	var c = confirm("Želiš najprej generirati nekaj pacientov?");
-	if(c) generirajPaciente(function(napaka){
-		
-	});
-
-	$("#prikazGraf").click(function(){
-		
-	})
+	google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawCrosshairs);
 
 	$("#pacient").change(function(){
 		var pacient = $(this).val();
 		var ehr;
 		switch(pacient){
 			case "Janez Bolan":
-				ehr = pacienti[0].ehrid; break;
+				ehr = pacienti.JanezBolan.ehrid; break;
 			case "Marija Kovač":
-				ehr = pacienti[1].ehrid; break;
+				ehr = pacienti.MarijaKovac.ehrid; break;
 			case "Matic Oblak":
-				ehr = pacienti[2].ehrid; break;
+				ehr = pacienti.MaticOblak.ehrid; break;
 		}
 		$("#ehrid_izpis").val(ehr);
 	})
@@ -320,9 +394,9 @@ $(function(){
 		var ehrid = $("#ehrid_vnos").val();
 		var sis = $("#sistolicni_vnos").val();
 		var dia = $("#diastolicni_vnos").val();
-		var polozaj = $("#polozaj").val();
+		var polozaj = "*Sitting(en)";
 		var datum = $("#datum_meritve").val();
-		vnosPodatkov(ehrid, sis, dia, polozaj, null, function(success){
+		vnosPodatkov(ehrid, sis, dia, polozaj, datum, function(success){
 			if(success){
 			}
 		});
@@ -333,7 +407,8 @@ $(function(){
 		var spol = $("#spol_vnos").val();
 		var priimek = $("#priimek").val();
 		var datum_rojstva = $("#datum_rojstva").val();
-		var ehr = novEHR(ime, priimek, spol, datum_rojstva);
+		novEHR(ime, priimek, spol, datum_rojstva, function(ehr){
+		});
 		console.log(ime + " " + priimek);
 	});
 	
